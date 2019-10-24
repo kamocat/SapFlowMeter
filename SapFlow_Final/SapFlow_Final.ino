c3f9bd76fd16bbf1998e6520ce7d568f80420829
Marshal Horn <kamocat@gmail.com>
2019-10-24 11:45:08 -0700

    Minimal non-loom implementation of state machine.
    Untested.

--- SapFlow_Final/SapFlow_Final.ino
+++ SapFlow_Final/SapFlow_Final.ino
@@ -6,13 +6,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #define ALARM_PIN 12
+#define HEATER 11
 
-#include <Loom.h>
-
-// Include configuration
-const char* json_config = 
-#include "config.h"
-;
+#include <OPEnS_RTC.h>
 
 enum state{
 	wake,
@@ -22,73 +18,67 @@ enum state{
 } measuring_state;
 
 
-// Set enabled modules
-LoomFactory<
-	Enable::Internet::Disabled,
-	Enable::Sensors::Disabled,
-	Enable::Radios::Disabled,
-	Enable::Actuators::Enabled,
-	Enable::Max::Disabled
-> ModuleFactory{};
+// Instance of our RTC
+RTC_DS3231 rtc_ds;
 
-LoomManager Loom{ &ModuleFactory };
-
-// Detach interrupt on wake
 volatile bool alarmFlag = false;
 void alarmISR() {
-  // Do I need to detach the interrupt every time, or can I just clear the alarm in the RTC?
-  Loom.InterruptManager().get_RTC_module()->clear_alarms();
+	// Do I need to detach the interrupt every time, or can I just clear the alarm in the RTC?
+	rtc_ds.clearAlarm(1);
+	alarmFlag = true;
+}
 
-  alarmFlag = true;
+void setTimer( int seconds ){
+	DateTime t = rtc_ds.now() + TimeSpan(seconds);
+	rtc_ds.setAlarm(ALM1_MATCH_HOURS, t.hour(), t.minute(), t.second());
 }
 
 void setup() 
 {
-  // Enable outputs to control 5V and 3.3V rails
-  pinMode(5, OUTPUT); pinMode(6, OUTPUT);
-  pinMode(ALARM_PIN, INPUT_PULLUP);
-  
-	Loom.begin_serial(true);
-	Loom.parse_config(json_config);
-	Loom.print_config();
-  measuring_state = wake;
+	// Enable outputs to control 5V and 3.3V rails
+	pinMode(5, OUTPUT); pinMode(6, OUTPUT);
+	pinMode(HEATER, OUTPUT);
+	pinMode(ALARM_PIN, INPUT_PULLUP);
+	measuring_state = wake;
+	
+	Serial.begin(115200);
 
-  Loom.InterruptManager().register_ISR(ALARM_PIN, alarmISR, LOW, ISR_Type::IMMEDIATE);
-  Loom.InterruptManager().RTC_alarm_duration(TimeSpan(10));
-
-	LPrintln("\n ** Setup Complete ** ");
-
+	// Falling-edge might not wake from sleep. Need more testing.
+	attachInterrupt(digitalPinToInterrupt(ALARM_PIN), alarmISR, LOW);
+	rtc_ds.armAlarm(1, true);
+	rtc_ds.alarmInterrupt(1, true);
+	Serial.println("\n ** Setup Complete ** ");
 }
 
 void loop() 
 {
-  Loom.pause(100);  // Slow down the loop a little
-  if (alarmFlag) {
-    alarmFlag = false;
-    switch(measuring_state){
-    	case wake:
-    		digitalWrite(5, LOW); digitalWrite(6, HIGH); // Enable 5V and 3.3V rails
-    		measuring_state = heating;
-    		Loom.Relay().set(true);
-        LPrintln("Heater On");
-    		//Set the alarm for heating time
-        Loom.InterruptManager().RTC_alarm_duration(TimeSpan(2)); 
-    		break;
-    	case heating:
-  			measuring_state = cooling;
-        Loom.Relay().set(false);
-  			//set the alarm for cooling time
-        Loom.InterruptManager().RTC_alarm_duration(TimeSpan(2)); 
-        LPrintln("Heater Off");
-    		break;
-    	default:
-        digitalWrite(5, HIGH); digitalWrite(6, LOW); // Disable 5V and 3.3V rails
-        measuring_state = wake;
-    		//set the alarm for sleep time
-        Loom.InterruptManager().RTC_alarm_duration(TimeSpan(2)); 
-    		LPrintln("Powering Down");
-    		//Sleep until interrupt
-        //Loom.SleepManager().sleep();
-    }
-  }
+	delay(100);	// Slow down the loop a little
+	if (alarmFlag) {
+		alarmFlag = false;
+		switch(measuring_state){
+			case wake:
+				digitalWrite(5, LOW); digitalWrite(6, HIGH); // Enable 5V and 3.3V rails
+				measuring_state = heating;
+				digitalWrite(HEATER, HIGH);
+				Serial.println("Heater On");
+				//Set the alarm for heating time
+				setTimer(2);
+				break;
+			case heating:
+				measuring_state = cooling;
+				digitalWrite(HEATER, LOW);
+				Serial.println("Heater Off");
+				//set the alarm for cooling time
+				setTimer(2);
+				break;
+			default:
+				// Disable 5V and 3.3V rails
+				digitalWrite(5, HIGH); digitalWrite(6, LOW); 
+				Serial.println("Powering Down");
+				measuring_state = wake;
+				//set the alarm for sleep time
+				setTimer(2);
+				//FIXME: Sleep until interrupt
+		}
+	}
 }
