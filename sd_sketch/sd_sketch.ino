05160418ec19279163ea94a7b75fc00031b15fdb
Marshal Horn <kamocat@gmail.com>
2019-10-26 16:43:49 -0700

    Correctly saving datapoints to CSV on SD card.

--- sd_sketch/sd_sketch.ino
+++ sd_sketch/sd_sketch.ino
@@ -23,11 +23,19 @@ class datapoint{
     sample1 = s1;
     sample2 = s2;
   }
+  // One version for printing to serial
   void print( ArduinoOutStream &stream, uint32_t offset){
     stream << setw(10) << (t - offset) << ',';
     stream << setw(6) << sample1 << ',';
     stream << setw(6) << sample2 << '\n';
   }
+  // Another version for printing to a file
+  void print( ofstream &stream, uint32_t offset){
+    stream << setw(10) << (t - offset) << ',';
+    stream << setw(6) << sample1 << ',';
+    stream << setw(6) << sample2 << '\n';
+  }
+  // A third version for printing to serial, just in case
   void print( void ){
     ArduinoOutStream s(Serial);
     print( s, 0 );
@@ -44,10 +52,19 @@ class datastream{
   std::vector <datapoint> data;
 
   public:
-  size_t dump( ArduinoOutStream cout ){
+  size_t dump( ArduinoOutStream &cout ){
     for( auto i = data.begin(); i != data.end(); ++i ){
       i->print(cout, t0);
     }
+    size_t k = data.size();
+    data.clear();
+    return k;
+  }
+  size_t dump( ofstream &f ){
+    for( auto i = data.begin(); i != data.end(); ++i ){
+      i->print(f, t0);
+    }
+    f<<flush;
     size_t k = data.size();
     data.clear();
     return k;
@@ -64,6 +81,25 @@ class datastream{
   }
 };
 
+// SD chip select pin.  Be sure to disable any other SPI devices such as Enet.
+const uint8_t chipSelect = 4;
+
+// Interval between data records in milliseconds.
+// The interval must be greater than the maximum SD write latency plus the
+// time to acquire and write data to the SD to avoid overrun errors.
+// Run the bench example to check the quality of your SD card.
+const uint32_t SAMPLE_INTERVAL_MS = 1000;
+
+// Log file base name.  Must be six characters or less.
+#define FILE_BASE_NAME "Data"
+//------------------------------------------------------------------------------
+// File system object.
+SdFat sd;
+
+// Data storage object
+datastream d;
+
+char * filename = "mytest2.csv";
 
 //------------------------------------------------------------------------------
 void setup(void) {
@@ -73,16 +109,27 @@ void setup(void) {
   while (!Serial) {
     SysCall::yield();
   }
+
+  // Initialize at the highest speed supported by the board that is
+  // not over 50 MHz. Try a lower speed if SPI errors occur.
+  if (!sd.begin(chipSelect, SD_SCK_MHZ(50))) {
+    sd.initErrorHalt();
+  }
+
+
   delay(2000);
-
-  datastream d;
-
-  for( int i = 0; i < 20; ++i ){
+  for( int i = 0; i < 10; ++i ){
     d.append(datapoint(i*1, i*2));
     delay(10);
   }
-  d.dump();
-
+  
+  // Sava data
+  Serial.print("Saving data... ");
+  ofstream sdout( filename );
+  d.dump( sdout );
+  sdout.close();
+  Serial.print("done.\n");
+}
+void loop(void) {
   
 }
-void loop(void) {}
