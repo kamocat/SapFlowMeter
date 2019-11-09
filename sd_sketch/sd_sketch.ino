81f586d191edec2bfb7d043bf3c68ffe9a96b146
Marshal Horn <kamocat@gmail.com>
2019-11-08 16:38:11 -0800

    Sketch is hanging in setAlarm function.

--- sd_sketch/sd_sketch.ino
+++ sd_sketch/sd_sketch.ino
@@ -6,141 +6,23 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #define ALARM_PIN 12
-#define HEATER 11
 
 #include <OPEnS_RTC.h>
 #include <LowPower.h>
-#include <SPI.h>
-#include "SdFat.h"
-#include "sdios.h"
-#include <Adafruit_MAX31865.h>
+#include <sdios.h> //for ArduinoOutStream
 
-Adafruit_MAX31865 rtd1 = Adafruit_MAX31865(A4);
-Adafruit_MAX31865 rtd2 = Adafruit_MAX31865(A5);
 
-#define RREF 430.0
-#define RNOMINAL 100.0
-// Adapt this datatype to your measurement needs.
-// SD chip select pin.  Be sure to disable any other SPI devices such as Enet.
-const uint8_t chipSelect = 10;
-
-// Interval between data records in milliseconds.
-// The interval must be greater than the maximum SD write latency plus the
-// time to acquire and write data to the SD to avoid overrun errors.
-// Run the bench example to check the quality of your SD card.
-const uint32_t SAMPLE_INTERVAL_MS = 1000;
-
-SdFat sd; // File system object.
-
-char * filename = "test5.txt";
 ArduinoOutStream cout(Serial);
-
-// Stores samples and relative time.
-// Adapt this datatype to your measurement needs.
-class sample{
-  private:
-  uint32_t t;  // milliseconds
-  float channel1;
-  float channel2;
-
-  public:
-  sample( Adafruit_MAX31865 r1, Adafruit_MAX31865 r2 ){
-    t = millis();
-    r1.readRTD();
-    r2.readRTD();
-    channel1 = r1.temperature(RNOMINAL, RREF);
-    channel2 = r2.temperature(RNOMINAL, RREF);
-  }
-  sample( size_t oversample ){
-    t = millis();
-    channel1 = 0;
-    channel2 = 0;
-    for( auto i = 0; i < oversample; ++i ){
-      channel1 += analogRead(A0);
-      channel2 += analogRead(A1);
-    }
-  }
-  sample( void ){
-    sample( 1 );
-  }
-  // One version for printing to serial
-  void print( ArduinoOutStream &stream){
-    stream << setw(6) << channel1 << ',';
-    stream << setw(6) << channel2 << '\n';
-  }
-  // Another version for printing to a file
-  void print( ofstream &stream){
-    stream << setw(6) << channel1 << ',';
-    stream << setw(6) << channel2 << '\n';
-  }
-  // A third version for printing to serial, just in case
-  void header( ofstream &stream ){
-    stream << setw(6) << "A0" << ',';
-    stream << setw(6) << "A1" << '\n';
-  }
-  uint32_t time( void ){
-    return t;
-  }
-};
-
-class datastream{
-  private:
-  uint32_t t0;  // milliseconds
-  DateTime date;
-  RTC_DS3231 * clock;
-  char * fname;
-  ofstream sdout;
-  bool file_open;
-  void timestamp( ofstream &cout, uint32_t sample_time ){
-    // Calculate milliseconds from start of dataset
-    uint32_t t = sample_time - t0;
-    // Offset date according to seconds elapsed
-    DateTime d = date + TimeSpan(t/1000);
-    t = t % 1000; // Calculate remainder milliseconds
-    // Print date and time with milliseconds
-    char old = cout.fill('0');
-    cout<<setw(4)<<d.year()<<'-';
-    cout<<setw(2)<<(int)d.month()<<'-'<<setw(2)<<(int)d.day()<<' ';
-    cout<<setw(2)<<(int)d.hour()<<':'<<setw(2)<<(int)d.minute()<<':';
-    cout<<setw(2)<<(int)d.second()<<'.'<<setw(3)<< t <<"  ,";
-    cout.fill(old);
-  }
-
-  public:
-  datastream( RTC_DS3231 &rtc_ds, char * filename ){
-    clock = &rtc_ds;
-    fname = filename;
-  }
-  void flush( void ){
-    sdout.flush();
-    sdout.close();
-    file_open = false;
-  }
-  void append( sample p ){
-    if( !file_open ){
-      date = clock->now();
-      t0 = p.time();
-      sdout = ofstream( fname, ios::out | ios::app );
-      file_open = true;
-    }
-    timestamp(sdout, p.time());
-    p.print(sdout);
-  }
-};
 
 
 // Instance of our RTC
 RTC_DS3231 rtc_ds;
 
-volatile uint32_t event_time;
-
-datastream d(rtc_ds, filename);
 
 void alarmISR() {
 	// Reset the alarm.
   rtc_ds.armAlarm(1, false);
 	rtc_ds.clearAlarm(1);
-	event_time = millis();
 }
 
 void printTime( DateTime t ){
@@ -210,18 +92,12 @@ void setup()
     // Set the RTC to the date & time this sketch was compiled
     rtc_ds.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-  if (!sd.begin(chipSelect, SD_SCK_MHZ(50))) {
-    Serial.println("SD card error");
-    sd.initErrorHalt();
-  }
   Serial.println("Setup complete");
 
 }
 void loop(void) {
   while(!Serial);
   Serial.println("Welcome!");
-  d.append(sample(1));
-  d.flush();
   setTimer(60);
   delay(1000);
   feather_sleep();
